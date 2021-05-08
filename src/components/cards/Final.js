50550b0a1e5d8c4c988d8d763264de4bcc2b1a8a
birodaniel8 <birodaniel8@gmail.com>
2021-05-08 14:27:33 +0200

    final card style added

--- src/components/cards/Final.js
+++ src/components/cards/Final.js
@@ -3,17 +3,20 @@ import { connect } from "react-redux";
 import { PropTypes } from "prop-types";
 import { Paper, Typography } from "@material-ui/core";
 
+import { useStyles } from "../../styles";
 import StartGameButton from "../StartGameButton";
 
 const Final = ({ settings, counter, cardPath }) => {
+  const classes = useStyles();
+
   const img_src = `${process.env.PUBLIC_URL}/sw_pics/${settings.target.replace("/", "-")}.png`;
   return (
-    <Paper>
+    <Paper className={classes.gameCard}>
       <Typography style={{ margin: "20px 0px", padding: "10px", fontWeight: "bold" }}>
-        Congratulations! You have got to {settings.target} in {counter} steps.
+        <p>Congratulations!</p> You have got to {settings.target} in {counter} steps.
       </Typography>
 
-      <img src={img_src} alt="" width="50%" />
+      <img src={img_src} alt="" width="50%" className={classes.finalImage} />
 
       <Typography style={{ marginTop: "20px", marginBottom: "10px" }}>Your path:</Typography>
 
--- src/styles.js
+++ src/styles.js
@@ -95,4 +95,9 @@ export const useStyles = makeStyles({
     marginBottom: "10px",
     fontSize: 15,
   },
+
+  // final card image:
+  finalImage: {
+    boxShadow: "0px 0px 2px 2px rgba(0, 0, 0, 0.2)",
+  }
 });
